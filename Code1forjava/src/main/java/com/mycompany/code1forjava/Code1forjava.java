5539264c1c827c9decc83bf933c0bd31036aa690
Tanawat <Tanawatkankhia2548@gmail.com>
2024-12-12 15:01:36 +0700

    init commit

---
+++ Code1forjava/src/main/java/com/mycompany/code1forjava/Code1forjava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.code1forjava;
+
+/**
+ *
+ * @author User
+ */
+public class Code1forjava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
